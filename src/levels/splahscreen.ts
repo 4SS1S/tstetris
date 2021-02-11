3242806a0f4aafc888566ecaa90475ba15cc32ee
Assis <assisguitar23@gmail.com>
2021-02-10 21:32:34 -0300

    level require

--- src/levels/splahscreen.ts
+++ src/levels/splahscreen.ts
@@ -2,7 +2,10 @@ import Level from "../scripts/level";
 
 export default class SplashScreen extends Level {
   constructor() {
-    super()
-    console.log('oi')
+    super();
+  }
+  
+  draw() {
+    console.log('ok')
   }
 }
--- src/scripts/level.ts
+++ src/scripts/level.ts
@@ -3,9 +3,17 @@ import Canvas from './canvas'
 export default class Level {
   constructor() {
     new Canvas().clearscreen();
+
+    window.requestAnimationFrame(this.draw)
   }
 
   loadLevel(level: Newable<any>) {
     return new level(this);
+  }
+
+  draw() {
+    setTimeout(() => {
+      console.log('ok')
+    }, 300)
   }
 }
